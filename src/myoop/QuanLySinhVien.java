c1cb702353517cc5729749d0b47b9a56b9d5e0ea
nguyenphuoc1506nhp <nguyenphuoc0000z@gmail.com>
2024-05-22 09:22:53 +0700

    Second Commit

---
+++ src/myoop/QuanLySinhVien.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package myoop;
+
+/**
+ *
+ * @author TGDD
+ */
+
+import lesson03.SinhVien;
+import lesson03.GiangVien;
+
+public class QuanLySinhVien {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        SinhVien[] sv;
+        GiangVien gva, gvb;
+    }
+    
+}
