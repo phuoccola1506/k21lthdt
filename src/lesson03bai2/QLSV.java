70289cc81124068c84487e46d7cc7b8e1d5e4241
nguyenphuoc1506nhp <nguyenphuoc0000z@gmail.com>
2024-05-24 08:13:29 +0700

    Bai 3 vidu 2

---
+++ src/lesson03bai2/QLSV.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lesson03bai2;
+
+/**
+ *
+ * @author TGDD
+ */
+public class QLSV {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        SinhVien sv = new  SinhVien();
+        sv.hoten = "Nguyen Van A";
+        sv.namsinh = -2005;
+        sv.sdt = "0123456789";
+        System.out.println(sv);
+    }
+    
+}
