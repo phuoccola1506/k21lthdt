9fdd68f3d0ade2ae5c35746b602aa4a65e14469e
nguyenphuoc1506nhp <nguyenphuoc0000z@gmail.com>
2024-06-06 23:44:58 +0700

    Bai tap 6

---
+++ src/lesson5/task1/DiChuyen.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package lesson5.task1;
+
+/**
+ *
+ * @author TGDD
+ */
+public interface DiChuyen {
+
+    public void sangTrai();
+
+    public void sangPhai();
+
+    public void batNhay();
+
+    public void boChay();
+}
---
+++ src/lesson5/task1/TanCong.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package lesson5.task1;
+
+/**
+ *
+ * @author TGDD
+ */
+public interface TanCong {
+    
+    public void tanCongCoVuKhi();
+    
+    public void tanCongKhongVuKhi();
+}
