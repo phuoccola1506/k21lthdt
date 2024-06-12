a33f3c8d0e6036e5fa8ea09387645aa93dc81c80
nguyenphuoc1506nhp <nguyenphuoc0000z@gmail.com>
2024-06-12 07:44:17 +0700

    Bai tap 8

---
+++ src/lesson8/DeepCloning.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lesson8;
+
+import java.util.ArrayList;
+import lesson8.vidu4.HoaDon;
+import lesson8.vidu4.SanPham;
+/**
+ *
+ * @author TGDD
+ */
+public class DeepCloning {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws CloneNotSupportedException {
+        // TODO code application logic here
+        ArrayList<SanPham> dssp = new ArrayList<>();
+        dssp.add(new SanPham(1, "Nokia"));
+        dssp.add(new SanPham(2, "SamSung"));
+        HoaDon hd1 = new HoaDon(0, dssp);
+        System.out.println(hd1);
+        HoaDon hd2 = (HoaDon)hd1.clone();
+    }
+    
+}
